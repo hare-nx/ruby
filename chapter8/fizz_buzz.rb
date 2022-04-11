e0d63c313a8052335f94d9adb7c4459cdd958a1d
EC2 Default User <ec2-user@ip-172-31-11-128.ec2.internal>
2022-04-11 09:10:31 +0000

    [Finish]2-8

--- chapter8/fizz_buzz.rb
+++ chapter8/fizz_buzz.rb
@@ -1,8 +1,13 @@
 def calc(num)
-    return "Fizz" if (num%3==0)&&(num%5!=0)
-    return "Buzz" if (num%5==0)&&(num%3!=0)
-    return "FizzBuzz" if (num%3==0)&&(num%5==0)
-    return "#{num}" if (num%3!=0)&&(num%5!=0)
+    if num%15==0
+        "FizzBuzz"
+    elsif num%3==0
+        "Fizz"
+    elsif num%5==0
+        "Buzz"
+    else
+        num.to_s
+    end
 end
 
 puts "数値を入力してください"
